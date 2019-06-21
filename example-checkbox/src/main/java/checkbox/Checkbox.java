909550bfe0e96dde8b6dbb4664facdce71b73d2f
Kevin Nhan <kbn.nhan@gmail.com>
2019-06-21 10:09:42 -0400

    Got checkboxes to work

--- example-checkbox/src/main/java/checkbox/Checkbox.java
+++ example-checkbox/src/main/java/checkbox/Checkbox.java
@@ -1,6 +1,18 @@
 package checkbox;
 
+import java.util.ArrayList;
+
 public class Checkbox {
+
+    private ArrayList<String> animals = new ArrayList<String>();
+
+    public void addAnimal(String animal) {
+        this.animals.add(animal); 
+    }
+
+    public ArrayList<String> getAnimals() {
+        return this.animals;
+    }
 
     
 }
--- example-checkbox/src/main/java/checkbox/CheckboxController.java
+++ example-checkbox/src/main/java/checkbox/CheckboxController.java
@@ -10,5 +10,24 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CheckboxController {
-    
+
+    @GetMapping("/checkbox")
+    public String quizForm(Model model) {
+        model.addAttribute("checkbox", new Checkbox());
+        return "checkbox";
+    }
+
+    @RequestMapping("/result")
+    public String getAnswerParams(
+        @RequestParam(name="box1", required=false) String animal1,
+        @RequestParam(name="box2", required=false) String animal2,
+        @RequestParam(name="box3", required=false) String animal3,
+        @ModelAttribute Checkbox checkbox) {
+
+            checkbox.addAnimal(animal1);
+            checkbox.addAnimal(animal2);
+            checkbox.addAnimal(animal3);
+            return "result";
+        }
+   
 }
